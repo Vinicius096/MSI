370ff0b2bbe8dcb4ada028e4f6cc2c3e9db603cb
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2020-08-10 11:30:21 -0300

    ToDo List featurecollector.py

---
+++ scripts/featurecollector.py
@@ -0,0 +1,9 @@
+#parametros: url do repositorio
+#passo 1: clonar um repositorio
+#passo 2: extrair as seguintes caracteristicas
+#A) linguagem
+#B) numero de commits que alterem um arquivo da linguagem
+#C) numero de devs que alteraram um arquivo da linguagem
+#D) numero de arquivos da linguagem
+#E) Truck Factor do repositório
+#passo 3: excluir o repositorio
